2fb19a22a84b6aa59d4e2c4b5d910a30f75731ff
codermahiuddin <codermahiuddin@gmail.com>
2020-08-09 00:25:56 +0600

    updated

--- main.js
+++ main.js
@@ -1,48 +1,61 @@
-function getRndInteger(min = 1000, max = 9999) {
-    return Math.floor(Math.random() * (max - min) ) + min;
-  }
 
- 
+  document.getElementById("generate").addEventListener("click", function(){
+    let randomNum = Math.floor(Math.random() * (9999 - 1000) ) + 1000;
+    document.getElementById('random-number').value = randomNum;
+    document.getElementById("input_num").value = "";
+  }) // set a random number and remove the user input value
+
  function removestr(){
     let str = document.getElementById("input_num").value
     let cstr = str.slice(0, -1)
     return cstr
- } 
-
+ }  // remove the last one by one
 
 document.getElementById("submit_btn").addEventListener("click",function(){
-    var usr_input = document.getElementById("input_num").value
-    var random_num = parseFloat(document.getElementById("random-number").value)
-    var number = parseFloat(usr_input)
+
+    let getUsrInput = document.getElementById("input_num")
+    let usr_input = getUsrInput.value
+    const random_num = parseFloat(document.getElementById("random-number").value)
+    const number = parseFloat(usr_input)
+    // paesefloat user input & random number
+
+    var ActionValue = document.getElementById("action_left") // try left action id
+    var TryCounter = document.getElementById("thisone")  // try left action value count id
 
     if (random_num == number) {
         document.getElementById("match").style.display="block"
         document.getElementById("notmatch").style.display="none"
-    } 
+
+        let tryCounterTxt =  TryCounter.innerText
+        let tryCounterInt = parseInt(tryCounterTxt)  // Count Pin Didn't Match Value
+        
+        if ( tryCounterInt == 1 || tryCounterInt == 2) {
+            ActionValue.innerHTML = "3 try left"
+            TryCounter.innerHTML = "0"
+        }  // close last if sta...... if ans false and then true. Return 3 try left again 
+
+    }  // close 1st if sta.....
+
     else{
 
         document.getElementById("notmatch").style.display="block"
         document.getElementById("match").style.display="none"
 
-            let add = document.getElementById("thisone");
-            let inside = add.innerHTML
+            let inside = TryCounter.innerText
             let number = parseInt(inside)
-            const larger = number + 1        
-            const getNum = add.innerHTML= larger
-        
+            let larger = number + 1        
+            const getNum = TryCounter.innerHTML= larger // add the how many time wrong ans
+            // count how much type wrong pin
+            
             if(getNum == 1){
-                document.getElementById("action_left").innerHTML = "2 try left"
+                ActionValue.innerHTML = "2 try left"
             }else if(getNum == 2){
-                document.getElementById("action_left").innerHTML = "1 try left"
+                ActionValue.innerHTML = "1 try left"
             }else if(getNum == 3){
-                document.getElementById("action_left").innerHTML = "0 try left"
-                document.getElementById("input_num").disabled = true;
+                ActionValue.innerHTML = "0 try left"
+                getUsrInput.disabled = true;
                 this.disabled = true;
-            }
-        
-    }
+            }      
+    } // close else sta......
+
 })
-
-
-
-
